fb5c2387724e406706ee640fa78f0f91d56c7ef0
Willy Stadnick <willy.stadnick@gmail.com>
2020-07-19 16:02:59 -0300

    Remove unused resources

--- tests/Browser/SearchTest.php
+++ tests/Browser/SearchTest.php
@@ -2,26 +2,12 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Pages\Search;
 use Tests\DuskTestCase;
 
 class ExampleTest extends DuskTestCase
 {
-    /**
-     * A basic browser test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee(env('APP_NAME'));
-        });
-    }
-
     public function testVisible()
     {
         $this->browse(function (Browser $browser) {
